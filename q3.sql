b6c166312db3f54ba96d26708a2054fd280b0613
Mateja Sela <msela@Matejas-MacBook-Pro.local>
2019-08-19 11:42:44 +0200

    finish SQL assignment

--- q3.sql
+++ q3.sql
@@ -1,4 +1,9 @@
 SELECT book.title
 FROM book
-JOIN checkout_item ON book.id = checkout_item.book_id
-WHERE book.id NOT IN checkout_item
+WHERE book.id NOT IN (
+SELECT DISTINCT checkout_item.book_id FROM checkout_item WHERE checkout_item.book_id IS NOT NULL);
+
+SELECT movie.title
+FROM movie
+WHERE movie.id NOT IN (
+SELECT DISTINCT checkout_item.movie_id FROM checkout_item WHERE checkout_item.movie_id IS NOT NULL);
